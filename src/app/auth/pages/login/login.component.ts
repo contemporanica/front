fd56b80a2bd18e0e4ccc35edb40ea0ce7d7c24dd
Pablodr89 <padoro89@gmail.com>
2023-02-23 20:09:33 +0100

    Generacion de formularios reactivos del login

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,Input,Output } from '@angular/core';
+import { FormControl,Validators,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  get email() {
+    return this.formUser.get('email') as FormControl;
+  }
+
+  get pass() {
+    return this.formUser.get('pass') as FormControl;
+  }
+
+  formUser = this.fb.group( {
+    'email': ['', [Validators.required, Validators.email]],
+    'pass': ['', Validators.required]
+  });
+
+  submit() {
+    console.log(this.formUser.value)
+  }
+
+  // @Input();
+
+  // @Output();
 }
