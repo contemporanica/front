9a61a5451dd173d24fc0c0c4243d904f3d7820ef
EvaCardenas <evacrdns17@gmail.com>
2023-02-24 22:14:25 +0100

    corregir variable

--- src/app/protected/components/search-bar/search-bar.component.ts
+++ src/app/protected/components/search-bar/search-bar.component.ts
@@ -11,13 +11,11 @@ export class SearchBarComponent {
   @Output() search: EventEmitter<string> = new EventEmitter<string>();
 
   //Recoger el valor del input cuando el usuario vaya escribiendo
-  onInputChange(event: any) {
-    console.log(event);
+  onInputChange(event: string) {
+
   }
   //Mandar el valor del input al componente padre
   onSearch() {
-
-    console.log(this.searchText);
     this.search.emit(this.searchText);
   }
 
