53dad4f7b08c380aaabec3f081378b03b2a9a335
pinguno <r4f4hc8@gmail.com>
2023-03-01 13:53:38 +0100

    T3-03_1 - Registro

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl,Validators,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  constructor(private _fb: FormBuilder) {
+
+  }
+
+  get nombre() {
+    return this.formUser.get('nombre') as FormControl;
+  }
+
+  get apellidos() {
+    return this.formUser.get('apellidos') as FormControl;
+  }
+
+  get email() {
+    return this.formUser.get('email') as FormControl;
+  }
+
+  get pass() {
+    return this.formUser.get('pass') as FormControl;
+  }
+
+  formUser = this._fb.group( {
+    'nombre': ['', Validators.required],
+    'email': ['', [Validators.required, Validators.email]],
+    'pass': ['', Validators.required],
+    'apellidos': ['', Validators.required]
+  });
+
+  submit() {
+    console.log(this.formUser.value)
+  }
+
+  // @Input();
+
+  // @Output();
 }
