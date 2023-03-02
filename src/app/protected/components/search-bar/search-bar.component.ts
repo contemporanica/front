1882c46f730e6c44c765b110102943d02c5d2846
EvaCardenas <evacrdns17@gmail.com>
2023-03-02 21:51:01 +0100

    implementacion peticion api

--- src/app/protected/components/search-bar/search-bar.component.ts
+++ src/app/protected/components/search-bar/search-bar.component.ts
@@ -1,31 +1,46 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { debounceTime, distinctUntilChanged, Observable, switchMap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent {
+
+
+export class SearchBarComponent implements OnInit{
 
   musicSheet: string = '';
   composer: string = '';
+  sugerencias: any[] = [];
 
   @Output() search: EventEmitter<string> = new EventEmitter<string>();
+  constructor(private http: HttpClient) { }
 
-  //Recoger el valor del input cuando el usuario vaya escribiendo
-  onInputChange(event: string) {
-  
-    console.log(event);
-
+  ngOnInit(): void {
   }
+
+  //Recoger el valor del input cuando el usuario vaya escribiendo y mostrar la lista de sugerencias
+  onInputChange(event: string): void {
+    if (event.length > 0) {
+      this.http.get<any[]>('url_api_contemporanica', {
+        params: {
+          q: event
+        }
+      }).pipe(
+        debounceTime(500)
+      ).subscribe((sugerencias) => {
+        this.sugerencias = sugerencias.slice(0, 5);
+      });
+    } else {
+      this.sugerencias = [];
+    }
+  }
+
   //Mandar el valor del input al componente padre
   onSearch() {
-
-    console.log(this.musicSheet);
-    console.log(this.composer);
-
     this.search.emit(this.musicSheet);
     this.search.emit(this.composer);
   }
-
 }
