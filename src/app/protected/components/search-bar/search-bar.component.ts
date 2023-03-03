4c0c40eca1256fbdd52360e0e0bdde98cb07408c
Pablodr89 <padoro89@gmail.com>
2023-03-03 11:13:30 +0100

    funcionalidad search

--- src/app/protected/components/search-bar/search-bar.component.ts
+++ src/app/protected/components/search-bar/search-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { debounceTime, distinctUntilChanged, Observable, switchMap } from 'rxjs';
+import { FormControl,Validators,FormBuilder } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { SearchServiceService } from 'src/app/shared/service/search-service.service';
 
 @Component({
   selector: 'app-search-bar',
@@ -9,28 +11,41 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class SearchBarComponent implements OnInit {
+
   musicSheet: string = '';
   composer: string = '';
   sugerencias: any[] = [];
+  query: string = '';
 
   @Output() search: EventEmitter<string> = new EventEmitter<string>();
-  constructor(private http: HttpClient) { } ngOnInit(): void { }
+
+  constructor(private http: HttpClient, private _fb: FormBuilder,  private _searchService: SearchServiceService) {
+
+  }
+
+  formUser = this._fb.group( {
+    'musicSheet': [''],
+    'composer': [''],
+    'sugerencias': ['']
+  })
+  ngOnInit(): void { }
 
   //Recoger el valor del input cuando el usuario vaya escribiendo y mostrar la lista de sugerencias 
-  onInputChange(event: string): void {
-    if (event.length > 0) {
-      this.http.get<any[]>('url_api_', {
-        params: {
-          q: event
-        }
-      }).pipe(debounceTime(500)).subscribe((sugerencias) => {
-        this.sugerencias = sugerencias.slice(0, 5);
-      });
+  onInputChange(): void {
+    if (this.query.length > 0) {
+      this._searchService.searchByCompositor(this.query).subscribe( resp => {
+        console.log(resp);
+      })
+      // }.pipe(debounceTime(500)).subscribe((sugerencias) => {
+      //   this.sugerencias = sugerencias.slice(0, 5);
+      // });
     } else { this.sugerencias = []; }
   }
+
   //Mandar el valor del input al componente padre 
   onSearch() {
     this.search.emit(this.musicSheet);
     this.search.emit(this.composer);
   }
+
 }
--- src/app/shared/service/search-service.service.ts
+++ src/app/shared/service/search-service.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Composer, Family, Instrument } from '../interface/interface';
@@ -8,22 +8,29 @@ import { Composer, Family, Instrument } from '../interface/interface';
 })
 export class SearchServiceService {
   private _apiUrl: String = 'http://contemporanica.com:4000/api/music';
+  private _tokenAPI: string = 'a944114ba4149684eb4e6b00e6093fdb';
   constructor(private _http: HttpClient) { }
 
   searchByCompositor( query: String ): Observable<Composer> {
-    const url = `${ this._apiUrl }/composer/name/${ query }`; 
+    const url = `${ this._apiUrl }/composer/name/${ query }`;
+    const headers = new HttpHeaders().set('token', this._tokenAPI);
     return this._http.get<Composer>( url );
   }
   searchByInstrument( query: String ): Observable<Instrument> {
-    const url = `${ this._apiUrl }/instrument/name/${ query }`; 
+    const url = `${ this._apiUrl }/instrument/name/${ query }`;
+    const headers = new HttpHeaders().set('token', this._tokenAPI);
     return this._http.get<Instrument>( url );
   }
   searchByPiece( query: String ): Observable<any> {
-    const url = `${ this._apiUrl }/name/name/${ query }`; 
+    const url = `${ this._apiUrl }/name/name/${ query }`;
+    const headers = new HttpHeaders().set('token', this._tokenAPI);
     return this._http.get( url );
   }
   searchByFamily( query: String ): Observable<Family> {
-    const url = `${ this._apiUrl }/family/name/${ query }`; 
+    const url = `${ this._apiUrl }/family/name/${ query }`;
+    const headers = new HttpHeaders().set('token', this._tokenAPI);
     return this._http.get<Family>( url );
   }
 }
+
+
